7d57fba0504eb9dcdbfac42716bbd3a790c6f14d
Evgenii Isupov <workline521@gmail.com>
2018-09-11 10:27:29 +0400

    removed slice function

--- js/map.js
+++ js/map.js
@@ -2,11 +2,9 @@
 
 var mapTest = [];
 
-var avatarImages = ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png'];
-var authorPictures = avatarImages.slice();
+var authorPictures = ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png'];
 
-var cardNames = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
-var titleNames = cardNames.slice();
+var titleNames = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
 
 var apartmentsTypes = ['palace', 'flat', 'house', 'bungalo'];
 
